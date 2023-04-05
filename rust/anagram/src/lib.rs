0889cae77c3e6707c55f7965e67cdb9002585f73
shaunkh <shaun.khundker@gmail.com>
2023-04-05 15:53:32 -0400

    meh idk asked for a mentor

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -4,19 +4,20 @@ use std::collections::HashSet;
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     let mut result: HashSet<&str> = HashSet::new();
     let mut word_map: HashMap<char, u32> = HashMap::new();
+    let lowercase_word = word.to_ascii_lowercase();
 
-    for l in 0..word.len() {
+    for l in 0..lowercase_word.len() {
         let mut count = 0;
-        for c in word.chars() {
-            if c.to_ascii_lowercase() == word.chars().nth(l).unwrap().to_ascii_lowercase() {
+        for c in lowercase_word.chars() {
+            if c == lowercase_word.chars().nth(l).unwrap() {
                 count += 1;
             }
         }
-        word_map.insert(word.chars().nth(l).unwrap(), count);
+        word_map.insert(lowercase_word.chars().nth(l).unwrap(), count);
     }
 
     for n in 0..possible_anagrams.len() {
-        if word != possible_anagrams[n] {
+        if lowercase_word != possible_anagrams[n].to_ascii_lowercase() {
             let mut anagram_map: HashMap<char, u32> = HashMap::new();
             for l in 0..possible_anagrams[n].len() {
                 let mut count = 0;
@@ -31,8 +32,17 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'
                         count += 1;
                     }
                 }
-                anagram_map.insert(possible_anagrams[n].chars().nth(l).unwrap(), count);
+                anagram_map.insert(
+                    possible_anagrams[n]
+                        .chars()
+                        .nth(l)
+                        .unwrap()
+                        .to_ascii_lowercase(),
+                    count,
+                );
             }
+            println!("{:?}", anagram_map);
+            println!("{:?}", anagram_map);
             if anagram_map == word_map {
                 result.insert(possible_anagrams[n]);
             }
