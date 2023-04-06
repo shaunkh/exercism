6b2e90d163d3b06725a54951fa14510fc3c4e7fb
shaunkh <shaun.khundker@gmail.com>
2023-04-06 08:24:33 -0400

    finish anagram

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -3,51 +3,25 @@ use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     let mut result: HashSet<&str> = HashSet::new();
-    let mut word_map: HashMap<char, u32> = HashMap::new();
-    let lowercase_word = word.to_ascii_lowercase();
+    let lowercase_word = word.to_lowercase();
+    let sorted_word = get_sorted(&lowercase_word);
 
-    for l in 0..lowercase_word.len() {
-        let mut count = 0;
-        for c in lowercase_word.chars() {
-            if c == lowercase_word.chars().nth(l).unwrap() {
-                count += 1;
-            }
+    for possible_anagram in possible_anagrams {
+        let lowercase_anagram = possible_anagram.to_lowercase();
+        if lowercase_word == lowercase_anagram {
+            continue;
         }
-        word_map.insert(lowercase_word.chars().nth(l).unwrap(), count);
-    }
 
-    for n in 0..possible_anagrams.len() {
-        if lowercase_word != possible_anagrams[n].to_ascii_lowercase() {
-            let mut anagram_map: HashMap<char, u32> = HashMap::new();
-            for l in 0..possible_anagrams[n].len() {
-                let mut count = 0;
-                for c in possible_anagrams[n].chars() {
-                    if c.to_ascii_lowercase()
-                        == possible_anagrams[n]
-                            .chars()
-                            .nth(l)
-                            .unwrap()
-                            .to_ascii_lowercase()
-                    {
-                        count += 1;
-                    }
-                }
-                anagram_map.insert(
-                    possible_anagrams[n]
-                        .chars()
-                        .nth(l)
-                        .unwrap()
-                        .to_ascii_lowercase(),
-                    count,
-                );
-            }
-            println!("{:?}", anagram_map);
-            println!("{:?}", anagram_map);
-            if anagram_map == word_map {
-                result.insert(possible_anagrams[n]);
-            }
+        let sorted_anagram = get_sorted(&lowercase_anagram);
+        if sorted_word == sorted_anagram {
+            result.insert(possible_anagram);
         }
     }
-
     result
+}
+
+fn get_sorted(word: &str) -> Vec<char> {
+    let mut word_sorted: Vec<char> = word.chars().collect();
+    word_sorted.sort_unstable();
+    word_sorted
 }
