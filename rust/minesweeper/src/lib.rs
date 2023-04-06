a3b703b2f1fbb14388694ae2946e38d2e28755c9
shaunkh <shaun.khundker@gmail.com>
2023-04-06 08:47:29 -0400

    minesweeper start

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    unimplemented!("\nAnnotate each square of the given minefield with the number of mines that surround said square (blank if there are no surrounding mines):\n{minefield:#?}\n");
+    let mut vec: Vec<String> = Vec::new();
+    for i in 0..minefield.len() {
+        let mut s = minefield[i].as_bytes();
+        for j in 0..s.len() {
+            let mut count = 0;
+            //bytes for empty char is 32
+            if (s[j] == 32) {}
+
+            //bytes for * is 42
+            if (s[j] == 42) {}
+        }
+    }
+
+    vec
 }
