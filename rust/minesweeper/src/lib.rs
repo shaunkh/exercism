c2ec719c0fad7d225dc1fb6ad57658edb4b0bea3
shaunkh <shaun.khundker@gmail.com>
2023-04-07 12:14:31 -0400

    minesweeper done

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,16 +1,98 @@
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
     let mut vec: Vec<String> = Vec::new();
+
     for i in 0..minefield.len() {
         let mut s = minefield[i].as_bytes();
+        vec.push(String::from(""));
+
         for j in 0..s.len() {
+            println!("{} {}", i, j);
             let mut count = 0;
             //bytes for empty char is 32
-            if (s[j] == 32) {}
+            if s[j] == 32 {
+                if isInBounds(i + 1, j, minefield) {
+                    if checkMine(i + 1, j, minefield) {
+                        count += 1;
+                    }
+                }
+                if isInBounds(i, j + 1, minefield) {
+                    if checkMine(i, j + 1, minefield) {
+                        count += 1;
+                    }
+                }
+                if isInBounds(i + 1, j + 1, minefield) {
+                    if checkMine(i + 1, j + 1, minefield) {
+                        count += 1;
+                    }
+                }
 
+                if i as i32 - 1 >= 0 {
+                    if isInBounds(i - 1, j, minefield) {
+                        if checkMine(i - 1, j, minefield) {
+                            count += 1;
+                        }
+                    }
+                    if isInBounds(i - 1, j + 1, minefield) {
+                        if checkMine(i - 1, j + 1, minefield) {
+                            count += 1;
+                        }
+                    }
+
+                    if j as i32 - 1 >= 0 {
+                        if isInBounds(i - 1, j - 1, minefield) {
+                            if checkMine(i - 1, j - 1, minefield) {
+                                count += 1;
+                            }
+                        }
+                    }
+                }
+
+                if j as i32 - 1 >= 0 {
+                    if isInBounds(i, j - 1, minefield) {
+                        if checkMine(i, j - 1, minefield) {
+                            count += 1;
+                        }
+                    }
+                    if isInBounds(i + 1, j - 1, minefield) {
+                        if checkMine(i + 1, j - 1, minefield) {
+                            count += 1;
+                        }
+                    }
+                }
+
+                if count == 0 {
+                    vec[i].push_str(" ");
+                } else {
+                    vec[i].push_str(&count.to_string());
+                }
+            }
             //bytes for * is 42
-            if (s[j] == 42) {}
+            else if s[j] == 42 {
+                vec[i].push_str("*");
+            }
         }
     }
 
     vec
+}
+
+pub fn isInBounds(i: usize, j: usize, minefield: &[&str]) -> bool {
+    if i < 0 || i > minefield.len() - 1 {
+        return false;
+    }
+
+    if j < 0 || j > minefield[i].len() - 1 {
+        return false;
+    }
+
+    true
+}
+
+pub fn checkMine(i: usize, j: usize, minefield: &[&str]) -> bool {
+    let s = minefield[i].as_bytes();
+    if s[j] == 42 {
+        return true;
+    }
+
+    false
 }
--- rust/minesweeper/tests/minesweeper.rs
+++ rust/minesweeper/tests/minesweeper.rs
@@ -56,7 +56,6 @@ fn board_with_only_mines() {
 }
 
 #[test]
-#[ignore]
 fn mine_surrounded_by_spaces() {
     #[rustfmt::skip]
     run_test(&[
@@ -67,7 +66,6 @@ fn mine_surrounded_by_spaces() {
 }
 
 #[test]
-#[ignore]
 fn space_surrounded_by_mines() {
     #[rustfmt::skip]
     run_test(&[
@@ -78,7 +76,6 @@ fn space_surrounded_by_mines() {
 }
 
 #[test]
-#[ignore]
 fn horizontal_line() {
     #[rustfmt::skip]
     run_test(&[
@@ -87,7 +84,6 @@ fn horizontal_line() {
 }
 
 #[test]
-#[ignore]
 fn horizontal_line_mines_at_edges() {
     #[rustfmt::skip]
     run_test(&[
@@ -96,7 +92,6 @@ fn horizontal_line_mines_at_edges() {
 }
 
 #[test]
-#[ignore]
 fn vertical_line() {
     #[rustfmt::skip]
     run_test(&[
@@ -109,7 +104,6 @@ fn vertical_line() {
 }
 
 #[test]
-#[ignore]
 fn vertical_line_mines_at_edges() {
     #[rustfmt::skip]
     run_test(&[
@@ -122,7 +116,6 @@ fn vertical_line_mines_at_edges() {
 }
 
 #[test]
-#[ignore]
 fn cross() {
     #[rustfmt::skip]
     run_test(&[
@@ -135,7 +128,6 @@ fn cross() {
 }
 
 #[test]
-#[ignore]
 fn large_board() {
     #[rustfmt::skip]
     run_test(&[
